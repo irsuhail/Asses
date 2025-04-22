289a32f9ff3055d8539f68533a0499d70d6c9b30
irsuhail <irsuhail2000@gmail.com>
2025-04-22 20:29:18 +0530

    Add files via upload

---
+++ userRegistration.js
@@ -0,0 +1,25 @@
+const EventEmitter = require('events');
+
+
+const eventEmitter = new EventEmitter();
+
+
+eventEmitter.on('userRegistered', (username, email) => {
+  console.log(`User ${username} has been successfully registered with email ${email}!`);
+
+  
+  setTimeout(() => {
+    eventEmitter.emit('sendConfirmationEmail', email);
+  }, 2000);
+});
+
+
+eventEmitter.on('sendConfirmationEmail', (email) => {
+  console.log(`Confirmation email sent to ${email}`);
+});
+
+
+const sampleUsername = 'JohnDoe';
+const sampleEmail = 'johndoe@example.com';
+
+eventEmitter.emit('userRegistered', sampleUsername, sampleEmail);
