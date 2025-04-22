6096db4124adce8c6fca63f8dfee22269ed5b0d1
irsuhail <irsuhail2000@gmail.com>
2025-04-22 20:26:30 +0530

    Add files via upload

---
+++ evenemitter.js
@@ -0,0 +1,13 @@
+
+const EventEmitter = require('events');
+
+
+const eventEmitter = new EventEmitter();
+
+
+eventEmitter.on('greet', () => {
+  console.log('Hello! Welcome to Event-Driven Programming in Node.js');
+});
+
+
+eventEmitter.emit('greet');
