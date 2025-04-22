057a461eeeaeb6619d144fba74636fd779f49468
irsuhail <irsuhail2000@gmail.com>
2025-04-22 21:15:04 +0530

    Initial commit from local Indicators folder

---
+++ Indicators/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+
+app.use(express.static('public'));
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    
+    socket.on('chat message', (msg) => {
+        console.log('Message received: ' + msg);
+       
+        io.emit('chat message', msg);
+    });
+
+    
+    socket.on('typing', (username) => {
+        
+        socket.broadcast.emit('typing', username);
+    });
+
+    
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
