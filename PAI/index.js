7aa5f30649c628d2acdff9854c03e10b3f8aa1e8
irsuhail <irsuhail2000@gmail.com>
2025-06-16 13:23:25 +0530

    Initial commit from local Problem1 folder

--- PAI/index.js
+++ PAI/index.js
@@ -6,7 +6,7 @@
 
  const app=express();
  app.use(express.json());
-app.use(logger);
+ app.use(logger);
 
 
 app.use("/users",userRouter);
--- Skill/routes/authRoutes.js
+++ Skill/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const router=express.Router();
 
-const {signup, login}=require('./controllers/authController');
+const {signup, login}=require('../controllers/authController');
 const validateSignup=require('./middlewares/validationMiddleware');
 router.post('/signup',validateSignup,signup);
 router.post('/login',login);
